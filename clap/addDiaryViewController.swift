980ca9493c7db8e629d81711b500dfd07d3d3dc9
omurayuki <g1221438@icloud.com>
2018-10-03 00:50:13 +0900

    ひとまずscrollViewの基本実装！

--- clap/addDiaryViewController.swift
+++ clap/addDiaryViewController.swift
@@ -1,22 +1,40 @@
 import UIKit
 
+
+
 class addDiaryViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
-
+    
     @IBOutlet weak var pageControl: UIPageControl!
+    
+    var contentWidth: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scrollView.delegate = self
-
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * 6, height:scrollView.frame.size.height)
+        
+        for i in 0 ..< 4 {
+            
+            let labelText = UILabel()
+            labelText.text = "\(i + 1)番目"
+            scrollView.addSubview(labelText)
+            
+            let xCoodinate = view.frame.midX + view.frame.width * CGFloat(i)
+            
+            contentWidth += view.frame.width
+            
+            labelText.frame = CGRect(x: xCoodinate - 50, y: view.frame.height / 2, width: 100, height: 100)
+        }
+        
+        
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
     }
     
-
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
+        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(414))
     }
     
 }
