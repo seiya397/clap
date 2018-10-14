04098eb502b47aa0dc7658ab1380b1dbe91482fe
omurayuki <g1221438@icloud.com>
2018-10-14 16:25:33 +0900

    replyTableViewCell.xib作成

---
+++ clap/xib/replyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  replyTableViewCell.swift
+//  clap
+//
+//  Created by オムラユウキ on 2018/10/14.
+//  Copyright © 2018 Seiya. All rights reserved.
+//
+
+import UIKit
+
+class replyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
