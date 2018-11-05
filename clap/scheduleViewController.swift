6382d6bc18c57830d3d9bf87ed853a22955603f9
Seiya <xoooseiyaooox@gmail.com>
2018-11-06 04:46:44 +0900

    プロフィール画像サイズ縮小 AppIcon追加

--- clap/scheduleViewController.swift
+++ clap/scheduleViewController.swift
@@ -73,13 +73,13 @@ class scheduleViewController: UIViewController {
     }()
     
     
-    @IBAction func nextTapped(_ sender:UIButton) {
-        calendar.setCurrentPage(getNextMonth(date: calendar.currentPage), animated: true)
-    }
-    
-    @IBAction  func previousTapped(_ sender:UIButton) {
-        calendar.setCurrentPage(getPreviousMonth(date: calendar.currentPage), animated: true)
-    }
+//    @IBAction func nextTapped(_ sender:UIButton) {
+//        calendar.setCurrentPage(getNextMonth(date: calendar.currentPage), animated: true)
+//    }
+//
+//    @IBAction  func previousTapped(_ sender:UIButton) {
+//        calendar.setCurrentPage(getPreviousMonth(date: calendar.currentPage), animated: true)
+//    }
     
     @IBAction func TapApp(_ sender: Any) {
         let eventShare = self.storyboard?.instantiateViewController(withIdentifier: "planController") as! planController
